9ce155e4e2870d45b039147eb1db292ca99334d9
David <53861457+SerugaDavid@users.noreply.github.com>
2023-02-22 16:14:30 +0100

    Added output to Main.java

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,9 @@ public class Main {
             }
         }
         if (cli.execPhase == Phase.LEX) {
+            for (var symbol : symbols) {
+                System.out.println(symbol.toString());
+            }
             return;
         }
     }

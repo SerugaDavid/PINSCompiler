f69ac5ced05fc0f7f214028a502cd19acdbc0dd5
David <53861457+SerugaDavid@users.noreply.github.com>
2023-03-28 14:24:59 +0200

    Implement name checking for expressions and definitions.

--- src/compiler/seman/name/NameChecker.java
+++ src/compiler/seman/name/NameChecker.java
@@ -18,6 +18,9 @@ import compiler.seman.common.NodeDescription;
 import compiler.seman.name.env.SymbolTable;
 import compiler.seman.name.env.SymbolTable.DefinitionAlreadyExistsException;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class NameChecker implements Visitor {
     /**
      * Opis vozlišč, ki jih povežemo z njihovimi
@@ -44,109 +47,279 @@ public class NameChecker implements Visitor {
 
     @Override
     public void visit(Call call) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Call expression
+         * Watch out for variable or type calls instead of function calls
+         */
+
+        Optional<Def> def = this.symbolTable.definitionFor(call.name);
+        if (def.isEmpty()) {
+            Report.error(call.position, "Function " + call.name + " is not defined. Called at: " + call.position);
+        }
+
+        // Check if it is a function call
+        if (def.get() instanceof VarDef) {
+            Report.error(call.position, "Variables are not callable. Called at: " + call.position);
+        } else if (def.get() instanceof TypeDef) {
+            Report.error(call.position, "Types are not callable. Called at: " + call.position);
+        } else {
+            // Handle if we need to check function parameters
+            // If not, ok...
+        }
     }
 
     @Override
     public void visit(Binary binary) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Binary expression
+         * Left and right expression will handle themselves in other visit methods
+         */
+
+        binary.left.accept(this);
+        binary.right.accept(this);
     }
 
     @Override
     public void visit(Block block) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Block expression
+         * Visit all expressions in the block
+         */
+
+        for (Expr expr : block.expressions) {
+            expr.accept(this);
+        }
     }
 
     @Override
     public void visit(For forLoop) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * For loop expression
+         * How do we handle counter? Probably is just defined in the where clause
+         */
+
+        forLoop.counter.accept(this);
+        forLoop.low.accept(this);
+        forLoop.high.accept(this);
+        forLoop.step.accept(this);
+        forLoop.body.accept(this);
     }
 
     @Override
     public void visit(Name name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Name expression
+         * Check if name is defined
+         */
+
+        Optional<Def> def = this.symbolTable.definitionFor(name.name);
+        if (def.isEmpty()) {
+            Report.error(name.position, "Name " + name + " is not defined. Called at: " + name.position);
+        }
+        // Handle what kind of definition is this and if its appropriate
+        // If this is even needed
     }
 
     @Override
     public void visit(IfThenElse ifThenElse) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * If then else expression
+         * Watch out for else expression because it may be optional
+         */
+
+        ifThenElse.condition.accept(this);
+        ifThenElse.thenExpression.accept(this);
+        Optional<Expr> elseExpr = ifThenElse.elseExpression;
+        if (elseExpr.isPresent()) {
+            elseExpr.get().accept(this);
+        }
     }
 
     @Override
     public void visit(Literal literal) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Literal expression
+         * Literals are fine, no need for name checking
+         */
     }
 
     @Override
     public void visit(Unary unary) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Unary expression
+         * Visit only one expression
+         */
+
+        unary.expr.accept(this);
     }
 
     @Override
     public void visit(While whileLoop) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * While loop expression
+         * Check condition and body
+         */
+
+        whileLoop.condition.accept(this);
+        whileLoop.body.accept(this);
     }
 
     @Override
     public void visit(Where where) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Where expression, Fun stuff (I guess not so fun, easy)
+         * We push the scope on our symbol table
+         * First we have to make all declarations in the where clause (handled by the defs visitor)
+         *  We declare all different definitions
+         *  Check for named types
+         * Visit the expression inside the where clause
+         * Pop the scope
+         */
+
+        this.symbolTable.pushScope();
+
+        where.defs.accept(this);
+        where.expr.accept(this);
+
+        this.symbolTable.popScope();
     }
 
     @Override
     public void visit(Defs defs) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Defs visitor
+         * First we add all the definitions to the symbol table
+         * Then we check all the definitions for type names
+         */
+
+        // Add definitions to symbol table.
+        for (Def def : defs.definitions) {
+            try {
+                this.symbolTable.insert(def);
+            } catch (DefinitionAlreadyExistsException e) {
+                Report.error(def.position, "Definition '" + def.name + "' already defined at: " + def.position);
+            }
+        }
+
+        // Check definitions.
+        for (Def def : defs.definitions) {
+            def.accept(this);
+        }
     }
 
     @Override
     public void visit(FunDef funDef) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Function definition
+         * First we check if the function type is defined
+         * We check parameter types
+         * Push scope
+         * Define parameters
+         * Visit body
+         * Pop scope
+         */
+
+        // check type definition
+        if (funDef.type instanceof TypeName) {
+            Optional<Def> def = this.symbolTable.definitionFor(((TypeName) funDef.type).identifier);
+            if (def.isEmpty()) {
+                Report.error(funDef.type.position, "Function type '" + ((TypeName) funDef.type).identifier + "' not defined for function '" + funDef.name + "'");
+            }
+            this.definitions.store(def.get(), funDef);
+        }
+
+        // check parameter type definitions
+        for (Parameter parameter: funDef.parameters) {
+            if (parameter.type instanceof TypeName) {
+                Optional<Def> def = this.symbolTable.definitionFor(((TypeName) parameter.type).identifier);
+                if (def.isEmpty()) {
+                    Report.error(parameter.type.position, "Parameter type '" + ((TypeName) parameter.type).identifier + "' not defined for parameter '" + parameter.name + "' in function '" + funDef.name + "'");
+                }
+            }
+        }
+
+        // tle se ga rekurzivno skustiš in pohendlaš te nove scope
+        this.symbolTable.pushScope();
+
+        for (Parameter parameter : funDef.parameters) {
+            try {
+                this.symbolTable.insert(parameter);
+            } catch (DefinitionAlreadyExistsException e) {
+                Report.error(parameter.position, "Parameter '" + parameter.name + "' already defined at: " + parameter.position + " in function '" + funDef.name + "'");
+            }
+        }
+        funDef.body.accept(this);
+
+        this.symbolTable.popScope();
     }
 
     @Override
     public void visit(TypeDef typeDef) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Type definition
+         * Check if type of type is defined
+         */
+
+        // check type definition
+        if (typeDef.type instanceof TypeName) {
+            Optional<Def> def = this.symbolTable.definitionFor(((TypeName) typeDef.type).identifier);
+            if (def.isEmpty()) {
+                Report.error(typeDef.type.position, "Type type '" + ((TypeName) typeDef.type).identifier + "' not defined for type '" + typeDef.name + "'");
+            }
+            this.definitions.store(def.get(), typeDef);
+        }
     }
 
     @Override
     public void visit(VarDef varDef) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Variable definition
+         * Check if type of variable is defined
+         */
+
+        // check type definition
+        if (varDef.type instanceof TypeName) {
+            Optional<Def> def = this.symbolTable.definitionFor(((TypeName) varDef.type).identifier);
+            if (def.isEmpty()) {
+                Report.error(varDef.type.position, "Variable type '" + ((TypeName) varDef.type).identifier + "' not defined for variable '" + varDef.name + "'");
+            }
+            this.definitions.store(def.get(), varDef);
+        }
     }
 
     @Override
     public void visit(Parameter parameter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Parameter definition
+         * IDK if this is needed
+         * Checks made in FunDef
+         */
     }
 
     @Override
     public void visit(Array array) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Array type
+         * IDK if this is needed
+         * Checks made in definitions
+         */
     }
 
     @Override
     public void visit(Atom atom) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Atom type
+         * IDK if this is needed
+         * Checks made in definitions
+         */
     }
 
     @Override
     public void visit(TypeName name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        /**
+         * Type name
+         * IDK if this is needed
+         * Checks made in definitions
+         */
     }
 }

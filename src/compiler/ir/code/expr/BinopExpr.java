40eb490b279d46c969d9a1e8af66fb3695840b90
David <53861457+SerugaDavid@users.noreply.github.com>
2023-05-04 12:18:59 +0200

    ConvertOp function

--- src/compiler/ir/code/expr/BinopExpr.java
+++ src/compiler/ir/code/expr/BinopExpr.java
@@ -5,6 +5,8 @@
 
 package compiler.ir.code.expr;
 
+import compiler.parser.ast.expr.Binary;
+
 import static common.RequireNonNull.requireNonNull;
 
 public class BinopExpr extends IRExpr {
@@ -34,5 +36,24 @@ public class BinopExpr extends IRExpr {
         ADD, SUB, MUL, DIV, MOD, // aritmetični
         AND, OR, // logični
         EQ, NEQ, LT, GT, LEQ, GEQ // primerjalni
+    }
+
+    public static Operator convertOp(Binary.Operator operator) {
+        return switch (operator) {
+            case ADD -> Operator.ADD;
+            case SUB -> Operator.SUB;
+            case MUL -> Operator.MUL;
+            case DIV -> Operator.DIV;
+            case MOD -> Operator.MOD;
+            case AND -> Operator.AND;
+            case OR -> Operator.OR;
+            case EQ -> Operator.EQ;
+            case NEQ -> Operator.NEQ;
+            case LT -> Operator.LT;
+            case GT -> Operator.GT;
+            case LEQ -> Operator.LEQ;
+            case GEQ -> Operator.GEQ;
+            default -> throw new IllegalArgumentException("Unknown operator: " + operator);
+        };
     }
 }
